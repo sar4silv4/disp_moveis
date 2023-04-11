bec3f2abfeac692858007e637d5a4cd9b1f44564
sar4silv4 <ferreiradasilvasara1302@gmail.com>
2023-04-11 14:24:00 -0400

    aula 11/04

--- lib/main.dart
+++ lib/main.dart
@@ -9,37 +9,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: PaginaInicial());
+    return MaterialApp(home: paginainicial());
   }
 }
 
-class PaginaInicial extends StatelessWidget {
-  const PaginaInicial({super.key});
+class paginainicial extends StatelessWidget {
+  const paginainicial({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Meu App"),
-        backgroundColor: Colors.red,
-      ),
-      body: Container(
-          height: 1000,
-          width: 1000,
+        appBar: AppBar(
+          title: Text("Titulo do App"),
+          backgroundColor: Color.fromARGB(110, 255, 27, 27),
+        ),
+        body: Container(
+          color: Color.fromARGB(255, 150, 146, 229),
           alignment: Alignment.center,
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             ElevatedButton(
               onPressed: () {},
-              child: Text(
-                "AVATAR",
-                style: TextStyle(fontSize: 50),
-              ),
+              child: Text("CLIQUE AQUI",
+                  style: TextStyle(
+                    fontSize: 30,
+                  )),
             ),
             ElevatedButton(
-              onPressed: (() {}),
-              child: Text("TITANIC"),
+              onPressed: () {},
+              child: Text("CLIQUE AQUI"),
             ),
-          ])),
-    );
+            Container(
+              height: 300,
+              color: Color.fromARGB(255, 232, 145, 230),
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("TITANIC"),
+              ),
+            ),
+            Container(
+              height: 300,
+              color: Color.fromARGB(255, 152, 235, 224),
+            ),
+          ]),
+        ));
   }
 }
