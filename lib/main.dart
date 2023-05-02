52bb22f567930bbd08191fb3cb0264c557078741
sar4silv4 <ferreiradasilvasara1302@gmail.com>
2023-05-02 13:13:07 -0400

    aula 27/04

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: paginainicial());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: paginainicial());
   }
 }
 
@@ -19,39 +22,78 @@ class paginainicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Titulo do App"),
-          backgroundColor: Color.fromARGB(110, 255, 27, 27),
-        ),
-        body: Container(
-          color: Color.fromARGB(255, 150, 146, 229),
-          alignment: Alignment.center,
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("CLIQUE AQUI",
-                  style: TextStyle(
-                    fontSize: 30,
-                  )),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("CLIQUE AQUI"),
-            ),
-            Container(
-              height: 300,
-              color: Color.fromARGB(255, 232, 145, 230),
-              alignment: Alignment.center,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text("TITANIC"),
+      appBar: AppBar(
+        title: Text("telegram"),
+        backgroundColor: Color.fromARGB(248, 12, 170, 218),
+        actions: [
+          IconButton(
+            onPressed: () {
+              print("O botão da camera foi clicado");
+            },
+            icon: Icon(Icons.camera_alt_outlined),
+          ),
+          IconButton(
+            onPressed: () {
+              print("O botão da busca foi clicado");
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              print("O botão do menu foi clicado");
+            },
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.chat),
+        backgroundColor: Color.fromARGB(248, 12, 170, 218),
+        onPressed: () {},
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: ListView(
+          children: [
+            ListTile(
+              onTap: () {
+                print("a conversa foi clicada");
+              },
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCCCHwlSMgWhEe_iXR8aXCeEFlL2KesdTzTvEirTQ2efpMAm4NANijxVzl8DwEuekaAhQ&usqp=CAU"),
               ),
+              title: Text("Fulaninho Alves"),
+              subtitle: Text("Oi, tudo bem?"),
+              trailing: Text("11:34"),
             ),
-            Container(
-              height: 300,
-              color: Color.fromARGB(255, 152, 235, 224),
+            ListTile(
+              onTap: () {
+                print("a conversa foi clicada");
+              },
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://s.yimg.com/ny/api/res/1.2/wvycRT1lgSSi54l4C35LPQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MA--/https://s.yimg.com/os/creatr-uploaded-images/2020-12/5eb92350-456f-11eb-abed-f4da586ebad8"),
+              ),
+              title: Text("Ciclaninho da Silva"),
+              subtitle: Text("Onde voce está?"),
+              trailing: Text("09:58"),
             ),
-          ]),
-        ));
+            ListTile(
+              onTap: () {
+                print("a conversa foi clicada");
+              },
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ35eggEqEcCOfbfVO_3FI7UGU0ryTcMCv0Rw&usqp=CAU"),
+              ),
+              title: Text("Beltraninho Souza"),
+              subtitle: Text("olá.. 🥰"),
+              trailing: Text("07:48"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
