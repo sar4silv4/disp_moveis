bdd8a2d043ddda96a42e803e2538f080e0c85644
sar4silv4 <127432663+sar4silv4@users.noreply.github.com>
2023-05-11 18:24:30 -0400

    Add files via upload

--- lib/chamadas.dart
+++ lib/chamadas.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
+
 import 'package:flutter/material.dart';
 
 class Chamadas extends StatelessWidget {
@@ -5,9 +7,85 @@ class Chamadas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Text("Pagina de chamadas"),
+    return ListView(
+      children: [
+        ListTile(
+          onTap: () {
+            print("a conversa foi clicada");
+          },
+          title: Text("Meu Status"),
+          subtitle: Text("Toque para atualizar seu status"),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage("https://i.pinimg.com/564x/a0/fb/86/a0fb86a2c694258ec3245c67ef3543ae.jpg"),
+          ),
+        ),
+
+        ListTile(
+           title: Text("Chamadas Recentes"),
+        ),
+
+        ListTile (
+           onTap: () {
+            print("a conversa foi clicada");
+          },
+
+          leading: CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 218, 218, 218),
+          ),
+          title: Row(
+            children: [
+               Text("WhatsApp ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 69, 105, 69)
+                    ),     
+                  ),
+                  SizedBox(width: 8,),
+              Icon(Icons.verified, 
+                   size: 18,
+                   color: Color.fromARGB(255, 71, 214, 75),
+              ),
+            ],
+          ),
+        ),
+
+          ListTile(
+          onTap: () {
+            print("a conversa foi clicada");
+          },
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://i.pinimg.com/564x/06/f2/24/06f22405602efcab1ff2af1558667422.jpg"),
+          ),
+          title: Text("Fulaninho Alves"),
+          subtitle: Text("Hoje 04:03"),
+        ),
+
+        ListTile(
+          onTap: () {
+            print("a conversa foi clicada");
+          },
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://i.pinimg.com/564x/9a/2f/ae/9a2fae8260ad402a10fe5ec6ab8558da.jpg"),
+          ),
+          title: Text("Ciclaninho da Silva"),
+          subtitle: Text("Ontem 09:11"),
+        ),
+
+        ListTile(
+          onTap: () {
+            print("a conversa foi clicada");
+          },
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://i.pinimg.com/564x/69/9e/17/699e17a0e9f4adbdfaf0682ffdf1f81a.jpg"),
+          ),
+          title: Text("Beltraninho Souza"),
+          subtitle: Text("Ontem 06:55"),
+
+        ),
+      ],
     );
   }
 }
--- lib/conversas.dart
+++ lib/conversas.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_print, prefer_const_constructors
+// ignore_for_file: avoid_print, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -7,48 +7,94 @@ class Conversas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: ListView(
-        children: [
-          ListTile(
-            onTap: () {
-              print("a conversa foi clicada");
-            },
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCCCHwlSMgWhEe_iXR8aXCeEFlL2KesdTzTvEirTQ2efpMAm4NANijxVzl8DwEuekaAhQ&usqp=CAU"),
-            ),
-            title: Text("Fulaninho Alves"),
-            subtitle: Text("Oi, tudo bem?"),
-            trailing: Text("11:34"),
+    return ListView(
+      children: [
+        ListTile(
+          onTap: () {
+            print("a conversa foi clicada");
+          },
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCCCHwlSMgWhEe_iXR8aXCeEFlL2KesdTzTvEirTQ2efpMAm4NANijxVzl8DwEuekaAhQ&usqp=CAU"),
           ),
-          ListTile(
-            onTap: () {
-              print("a conversa foi clicada");
-            },
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://s.yimg.com/ny/api/res/1.2/wvycRT1lgSSi54l4C35LPQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MA--/https://s.yimg.com/os/creatr-uploaded-images/2020-12/5eb92350-456f-11eb-abed-f4da586ebad8"),
-            ),
-            title: Text("Ciclaninho da Silva"),
-            subtitle: Text("Onde voce está?"),
-            trailing: Text("09:58"),
+          title: Text("Fulaninho Alves"),
+          subtitle: Text("bão?"),
+          trailing: Column(
+            children: [
+              Text(
+                "11:34",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 36, 224, 42),
+                ),
+              ),
+              SizedBox(height: 6),
+              CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 36, 224, 42),
+                radius: 12,
+                child: Text("2",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+            ],
           ),
-          ListTile(
-            onTap: () {
-              print("a conversa foi clicada");
-            },
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ35eggEqEcCOfbfVO_3FI7UGU0ryTcMCv0Rw&usqp=CAU"),
-            ),
-            title: Text("Beltraninho Souza"),
-            subtitle: Text("olá.. 🥰"),
-            trailing: Text("07:48"),
+        ),
+        ListTile(
+          onTap: () {
+            print("a conversa foi clicada");
+          },
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://s.yimg.com/ny/api/res/1.2/wvycRT1lgSSi54l4C35LPQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MA--/https://s.yimg.com/os/creatr-uploaded-images/2020-12/5eb92350-456f-11eb-abed-f4da586ebad8"),
           ),
-        ],
-      ),
+          title: Text("Ciclaninho da Silva"),
+          subtitle: Row(
+            children: [
+              Icon(
+                Icons.done_all,
+                size: 18,
+                color: Color.fromARGB(255, 0, 136, 204),
+              ),
+              Text("Cade vc?"),
+            ],
+          ),
+          trailing: Text("09:58"),
+        ),
+        ListTile(
+          onTap: () {
+            print("a conversa foi clicada");
+          },
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ35eggEqEcCOfbfVO_3FI7UGU0ryTcMCv0Rw&usqp=CAU"),
+          ),
+          title: Text("Beltraninho Souza"),
+          subtitle: Text("iae mano?"),
+          trailing: Column(
+            children: [
+              Text(
+                "11:34",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 36, 224, 42),
+                ),
+              ),
+              SizedBox(height: 6),
+              CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 36, 224, 42),
+                radius: 12,
+                child: Text("2",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
